e3e1c94af34d8497f915e86496199e190b3b76bc
afzal.arsalan@gmail.com <afzal.arsalan@gmail.com@03dedc1a-bcf9-4cce-036f-12a59c4ec702>
2014-05-12 05:02:47 +0000

    Initial MainMenu Code

---
+++ SuperMarioNES/trunk/src/com/mario/Main.java
@@ -0,0 +1,10 @@
+package com.mario;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("This a not test string");
+        System.out.print("Another test string");
+    }
+}
