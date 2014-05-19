e4510c8d46e375f035a9366def3164c5371d1967
afzal.arsalan@gmail.com <afzal.arsalan@gmail.com@03dedc1a-bcf9-4cce-036f-12a59c4ec702>
2014-05-19 03:12:07 +0000

    Trying out a new method for running our game that doesn't involve rewriting all the code into arrays

--- SuperMarioNES/trunk/src/com/mario/Koompa.java
+++ SuperMarioNES/trunk/src/com/mario/Koompa.java
@@ -6,15 +6,16 @@
 
 package com.mario;
 
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
 
 /**
  *
  * @author lathasuraparaju
  */
+
 public class Koompa {
 
     /**
@@ -23,21 +24,30 @@ public class Koompa {
     public static void main(String[] args) {
         // TODO code application logic here
          JFrame j=new JFrame();
-        KompaTrooper obj=new KompaTrooper(100,10);
+        KoompaTrooper obj=new KoompaTrooper(100,10);
 j.setSize(obj.getSize());
 j.setBackground(Color.WHITE);
 j.add(obj);
 j.setVisible(true);
-j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+j.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
     
 }
-class KompaTrooper extends JPanel implements ActionListener{
+
+class KoompaTrooper extends JPanel implements ActionListener{
     int x,y;
-    public KompaTrooper(int x,int y){
+    private int red;
+    private int green;
+    private int blue;
+
+    public KoompaTrooper(int x, int y){
         this.x=x;
         this.y=y;
-         setSize(1000,1000);
+        setSize(1000,1000);
+        red = (int)(Math.random()*256);
+        green = (int)(Math.random()*256);
+        blue = (int)(Math.random()*256);
+
     }
     public void paintComponent(Graphics g){
          super.paintComponent(g);
@@ -46,242 +56,241 @@ class KompaTrooper extends JPanel implements ActionListener{
        
        
     }
+
+    @Deprecated
     public void MovingLeft1(Graphics g){
         Graphics2D g1=(Graphics2D)g;
         g1.scale(2, 2);
-        int r=(int)(Math.random()*256);
+        /*int r=(int)(Math.random()*256);
         int g2=(int)(Math.random()*256);
-        int b=(int)(Math.random()*256);
-         // body color shade
-                   g.setColor(new Color(233,171,116));
-                   g.fillRect(x-10, y+35,5,25 );
-                   g.fillRect(x-5, y+44,5,11 );
-                   g.fillRect(x, y+39,5,5 );
-                    g.fillRect(x+5, y+34,5,5 );
-                    g.fillRect(x+5, y+103,5,10 );
-                    g.fillRect(x, y+108,5,10);
-                    g.fillRect(x+40, y+113,5,5);
-                    g.fillRect(x+20, y+25,5,5);//this
-                    g.fillRect(x+15, y+20,5,5);//this
-                    //body color
-                    g.setColor(new Color(171,86,0));
-                     g.fillRect(x-5, y+35,5,5 );
-                     g.fillRect(x+15, y+25,5,25 );
-                     g.fillRect(x+20, y+30,5,10 );
-                     g.fillRect(x+10, y+25,5,34 );
-                     g.fillRect(x+35, y+113,5,5);
-                     g.fillRect(x, y+44,3,11 );
-                     g.fillRect(x-5, y+44,9,6 );
-                     g.fillRect(x+5, y+39,5,5 );
-                     g.fillRect(x+7, y+48,5,11 );
-                     g.fillRect(x-5, y+55,18,16 );
-                     g.fillRect(x-5, y+55,5,10 );
-                     g.fillRect(x+15, y+20,5,5);
-                      g.fillRect(x+4, y+44,6,6);
-                      g.fillRect(x-1, y+69,11,9);
-                      g.fillRect(x+2, y+78,8,8);
-                       g.fillRect(x+10, y+108,10,10);
-                       g.fillRect(x+5, y+113,5,5);
-                       g.fillRect(x-5, y+115,5,3);
-                       //shell
-                       g.setColor(new Color(r,g2,b));
-                       g.fillRect(x+30, y+40,10 ,10);
-                       g.fillRect(x+15, y+50,35 ,55);
-                       g.fillRect(x+15, y+50,35 ,55);
-                       g.fillRect(x+10, y+70,10 ,30);
-                       g.fillRect(x+45, y+70,10 ,30);
-                       g.fillRect(x+20, y+100,30 ,10);
-                       
+        int b=(int)(Math.random()*256);*/
+        // body color shade
+        g.setColor(new Color(233,171,116));
+        g.fillRect(x-10, y+35,5,25 );
+        g.fillRect(x-5, y+44,5,11 );
+        g.fillRect(x, y+39,5,5 );
+        g.fillRect(x+5, y+34,5,5 );
+        g.fillRect(x+5, y+103,5,10 );
+        g.fillRect(x, y+108,5,10);
+        g.fillRect(x+40, y+113,5,5);
+        g.fillRect(x+20, y+25,5,5);//this
+        g.fillRect(x+15, y+20,5,5);//this
+        //body color
+        g.setColor(new Color(171,86,0));
+        g.fillRect(x-5, y+35,5,5 );
+        g.fillRect(x+15, y+25,5,25 );
+        g.fillRect(x+20, y+30,5,10 );
+        g.fillRect(x+10, y+25,5,34 );
+        g.fillRect(x+35, y+113,5,5);
+        g.fillRect(x, y+44,3,11 );
+        g.fillRect(x-5, y+44,9,6 );
+        g.fillRect(x+5, y+39,5,5 );
+        g.fillRect(x+7, y+48,5,11 );
+        g.fillRect(x-5, y+55,18,16 );
+        g.fillRect(x-5, y+55,5,10 );
+        g.fillRect(x+15, y+20,5,5);
+        g.fillRect(x+4, y+44,6,6);
+        g.fillRect(x-1, y+69,11,9);
+        g.fillRect(x+2, y+78,8,8);
+        g.fillRect(x+10, y+108,10,10);
+        g.fillRect(x+5, y+113,5,5);
+        g.fillRect(x-5, y+115,5,3);
+        //shell
+        g.setColor(new Color(red,green,blue));
+        g.fillRect(x+30, y+40,10 ,10);
+        g.fillRect(x+15, y+50,35 ,55);
+        g.fillRect(x+15, y+50,35 ,55);
+        g.fillRect(x+10, y+70,10 ,30);
+        g.fillRect(x+45, y+70,10 ,30);
+        g.fillRect(x+20, y+100,30 ,10);
+
         //outline
-     //eye
+        //eye
         g.setColor(Color.BLACK);
         g.fillRect(x, y,10,5 );
         g.fillRect(x-5, y+5,5,10 );
         g.fillRect(x-10, y+15,5,20 );
         g.fillRect(x-5, y+19,5,10 );
-         g.fillRect(x+10, y+5,5,20 );
-         //head
-         g.fillRect(x+15, y+15, 5, 5);
-          g.fillRect(x+20, y+20,5,5 );
-          g.fillRect(x+25, y+25,5,15 );
-          g.fillRect(x+20, y+40,5,10 );
-          g.fillRect(x+15, y+50,5,8 );
-           g.fillRect(x+10, y+58,5,15 );
-            g.fillRect(x+5, y+73,5,30 );
-            g.fillRect(x+10, y+98,5,5 );
-            g.fillRect(x+10, y+103,10,5 );
-                g.fillRect(x+20, y+108,30,5 );
-                 g.fillRect(x+50, y+98,5,10 );
-                  g.fillRect(x-15, y+35,5,25 );
-                   g.fillRect(x-10, y+60,5,5 );
-                   g.fillRect(x-5, y+65,4,10 );
-                    g.fillRect(x-1, y+55,4,10 );
-                    g.fillRect(x+3, y+50,4,7 );
-                    g.fillRect(x-5, y+39,5,5 );
-                     g.fillRect(x-1, y+75,4,10 );
-                     g.fillRect(x+2, y+85,5,5 );
-                 
-                 //legs
-                 g.fillRect(x, y+103,5,5 );
-                 g.fillRect(x-5, y+108,5,5 );
-                 g.fillRect(x-10, y+113,5,5 );
-                 g.fillRect(x-10, y+118,30,2 );
-                 g.fillRect(x+20, y+108,5,12 );
-                 g.fillRect(x+30, y+108,5,11 );
-                 g.fillRect(x+30, y+118,20,2 );
-                 g.fillRect(x+50, y+112,4,8 );
-                 //shell
-                 g.fillRect(x+30, y+40,10,5 );
-                  g.fillRect(x+40, y+45,5,5 );
-                   g.fillRect(x+25, y+45,5,5 );
-                   g.fillRect(x+45, y+50,5,10 );
-                   g.fillRect(x+50, y+60,5,20 );
-                   g.fillRect(x+55, y+80,5,18 );
-               
-                   g.fillRect(x+45, y+65,5,5 );
-                   g.fillRect(x+25, y+85,5,5 );
-                   g.fillRect(x+30, y+80,5,5 );
-                   g.fillRect(x+35, y+75,5,5 );
-                   g.fillRect(x+40, y+70,5,5 );
-                   g.fillRect(x+20, y+90,5,5 );
-                   g.fillRect(x+15, y+95,5,5 );
-                    g.fillRect(x+45, y+93,5,5 );
-                      g.fillRect(x+40, y+88,5,5 );
-                        g.fillRect(x+35, y+83,5,5 );
-                        g.fillRect(x+30, y+78,5,5 );
-                        g.fillRect(x+25, y+73,5,5 );
-                        g.fillRect(x+20, y+68,5,5 );
-                        g.fillRect(x+15, y+63,5,5 );
-                         g.fillRect(x+10, y+58,5,5 );
-                        g.fillRect(x+15, y+43,5,5 );
-                    
-                   
-                  
-                     
-                    
-                 
+        g.fillRect(x+10, y+5,5,20 );
+        //head
+        g.fillRect(x+15, y+15, 5, 5);
+        g.fillRect(x+20, y+20,5,5 );
+        g.fillRect(x+25, y+25,5,15 );
+        g.fillRect(x+20, y+40,5,10 );
+        g.fillRect(x+15, y+50,5,8 );
+        g.fillRect(x+10, y+58,5,15 );
+        g.fillRect(x+5, y+73,5,30 );
+        g.fillRect(x+10, y+98,5,5 );
+        g.fillRect(x+10, y+103,10,5 );
+        g.fillRect(x+20, y+108,30,5 );
+        g.fillRect(x+50, y+98,5,10 );
+        g.fillRect(x-15, y+35,5,25 );
+        g.fillRect(x-10, y+60,5,5 );
+        g.fillRect(x-5, y+65,4,10 );
+        g.fillRect(x-1, y+55,4,10 );
+        g.fillRect(x+3, y+50,4,7 );
+        g.fillRect(x-5, y+39,5,5 );
+        g.fillRect(x-1, y+75,4,10 );
+        g.fillRect(x+2, y+85,5,5 );
+
+        //legs
+        g.fillRect(x, y+103,5,5 );
+        g.fillRect(x-5, y+108,5,5 );
+        g.fillRect(x-10, y+113,5,5 );
+        g.fillRect(x-10, y+118,30,2 );
+        g.fillRect(x+20, y+108,5,12 );
+        g.fillRect(x+30, y+108,5,11 );
+        g.fillRect(x+30, y+118,20,2 );
+        g.fillRect(x+50, y+112,4,8 );
+        //shell
+        g.fillRect(x+30, y+40,10,5 );
+        g.fillRect(x+40, y+45,5,5 );
+        g.fillRect(x+25, y+45,5,5 );
+        g.fillRect(x+45, y+50,5,10 );
+        g.fillRect(x+50, y+60,5,20 );
+        g.fillRect(x+55, y+80,5,18 );
+
+        g.fillRect(x+45, y+65,5,5 );
+        g.fillRect(x+25, y+85,5,5 );
+        g.fillRect(x+30, y+80,5,5 );
+        g.fillRect(x+35, y+75,5,5 );
+        g.fillRect(x+40, y+70,5,5 );
+        g.fillRect(x+20, y+90,5,5 );
+        g.fillRect(x+15, y+95,5,5 );
+        g.fillRect(x+45, y+93,5,5 );
+        g.fillRect(x+40, y+88,5,5 );
+        g.fillRect(x+35, y+83,5,5 );
+        g.fillRect(x+30, y+78,5,5 );
+        g.fillRect(x+25, y+73,5,5 );
+        g.fillRect(x+20, y+68,5,5 );
+        g.fillRect(x+15, y+63,5,5 );
+        g.fillRect(x+10, y+58,5,5 );
+        g.fillRect(x+15, y+43,5,5 );
+
+
+
+
+
+
     }
-public void MovingLeft2(Graphics g){
+
+    public void MovingLeft2(Graphics g){
         Graphics2D g1=(Graphics2D)g;
-        g1.scale(2,2);
-        int r=(int)(Math.random()*256);
+        g1.scale(0.5,0.5);
+        /*int r=(int)(Math.random()*256);
         int g2=(int)(Math.random()*256);
-        int b=(int)(Math.random()*256);
-         // body color shade
-                   g.setColor(new Color(233,171,116));
-                   g.fillRect(x-10, y+35,5,25 );
-                   g.fillRect(x-5, y+44,5,11 );
-                   g.fillRect(x, y+39,5,5 );
-                    g.fillRect(x+5, y+34,5,5 );
-                    g.fillRect(x+5, y+103,5,10 );
-                    g.fillRect(x, y+108,5,10);
-                    g.fillRect(x+40, y+113,5,5);
-                    g.fillRect(x+20, y+25,5,5);//this
-                    g.fillRect(x+15, y+20,5,5);//this
-                    //body color
-                    g.setColor(new Color(171,86,0));
-                     g.fillRect(x-5, y+35,5,5 );
-                     g.fillRect(x+15, y+25,5,25 );
-                     g.fillRect(x+20, y+30,5,10 );
-                     g.fillRect(x+10, y+25,5,34 );
-                     g.fillRect(x+35, y+113,5,5);
-                     g.fillRect(x, y+44,3,11 );
-                     g.fillRect(x-5, y+44,9,6 );
-                     g.fillRect(x+5, y+39,5,5 );
-                     g.fillRect(x+7, y+48,5,11 );
-                     g.fillRect(x-5, y+55,18,16 );
-                     g.fillRect(x-5, y+55,5,10 );
-                     g.fillRect(x+15, y+20,5,5);
-                      g.fillRect(x+4, y+44,6,6);
-                      g.fillRect(x-1, y+69,11,9);
-                      g.fillRect(x+2, y+78,8,8);
-                       g.fillRect(x+10, y+108,10,10);
-                       g.fillRect(x+5, y+113,5,5);
-                       g.fillRect(x-5, y+115,5,3);
-                       //shell
-                       g.setColor(new Color(r,g2,b));
-                       g.fillRect(x+30, y+40,10 ,10);
-                       g.fillRect(x+15, y+50,35 ,55);
-                       g.fillRect(x+15, y+50,35 ,55);
-                       g.fillRect(x+10, y+70,10 ,30);
-                       g.fillRect(x+45, y+70,10 ,30);
-                       g.fillRect(x+20, y+100,30 ,10);
-                       
+        int b=(int)(Math.random()*256);*/
+        // body color shade
+        g.setColor(new Color(233,171,116));
+        g.fillRect(x-10, y+35,5,25 );
+        g.fillRect(x-5, y+44,5,11 );
+        g.fillRect(x, y+39,5,5 );
+        g.fillRect(x+5, y+34,5,5 );
+        g.fillRect(x+5, y+103,5,10 );
+        g.fillRect(x, y+108,5,10);
+        g.fillRect(x+40, y+113,5,5);
+        g.fillRect(x+20, y+25,5,5);//this
+        g.fillRect(x+15, y+20,5,5);//this
+        //body color
+        g.setColor(new Color(171,86,0));
+        g.fillRect(x-5, y+35,5,5 );
+        g.fillRect(x+15, y+25,5,25 );
+        g.fillRect(x+20, y+30,5,10 );
+        g.fillRect(x+10, y+25,5,34 );
+        g.fillRect(x+35, y+113,5,5);
+        g.fillRect(x, y+44,3,11 );
+        g.fillRect(x-5, y+44,9,6 );
+        g.fillRect(x+5, y+39,5,5 );
+        g.fillRect(x+7, y+48,5,11 );
+        g.fillRect(x-5, y+55,18,16 );
+        g.fillRect(x-5, y+55,5,10 );
+        g.fillRect(x+15, y+20,5,5);
+        g.fillRect(x+4, y+44,6,6);
+        g.fillRect(x-1, y+69,11,9);
+        g.fillRect(x+2, y+78,8,8);
+        g.fillRect(x+10, y+108,10,10);
+        g.fillRect(x+5, y+113,5,5);
+        g.fillRect(x-5, y+115,5,3);
+        //shell
+        g.setColor(new Color(red,green,blue));
+        g.fillRect(x+30, y+40,10 ,10);
+        g.fillRect(x+15, y+50,35 ,55);
+        g.fillRect(x+15, y+50,35 ,55);
+        g.fillRect(x+10, y+70,10 ,30);
+        g.fillRect(x+45, y+70,10 ,30);
+        g.fillRect(x+20, y+100,30 ,10);
+
         //outline
-     //eye
+        //eye
         g.setColor(Color.BLACK);
         g.fillRect(x, y,10,5 );
         g.fillRect(x-5, y+5,5,10 );
         g.fillRect(x-10, y+15,5,20 );
         g.fillRect(x-5, y+19,5,10 );
-         g.fillRect(x+10, y+5,5,20 );
-         //head
-         g.fillRect(x+15, y+15, 5, 5);
-          g.fillRect(x+20, y+20,5,5 );
-          g.fillRect(x+25, y+25,5,15 );
-          g.fillRect(x+20, y+40,5,10 );
-          g.fillRect(x+15, y+50,5,8 );
-           g.fillRect(x+10, y+58,5,15 );
-            g.fillRect(x+5, y+73,5,30 );
-            g.fillRect(x+10, y+98,5,5 );
-            g.fillRect(x+10, y+103,10,5 );
-                g.fillRect(x+20, y+108,30,5 );
-                 g.fillRect(x+50, y+98,5,10 );
-                  g.fillRect(x-15, y+35,5,25 );
-                   g.fillRect(x-10, y+60,5,5 );
-                   g.fillRect(x-5, y+65,4,10 );
-                    g.fillRect(x-1, y+55,4,10 );
-                    g.fillRect(x+3, y+50,4,7 );
-                    g.fillRect(x-5, y+39,5,5 );
-                     g.fillRect(x-1, y+75,4,10 );
-                     g.fillRect(x+2, y+85,5,5 );
-                 
-                 //legs
-                    
-                 g.fillRect(x, y+103,5,5 );
-                 g.fillRect(x-5, y+108,5,5 );
-                 g.fillRect(x-10, y+113,5,5 );
-                  g.fillRect(x+15, y+98,5,5 );
-                 
-                 
-                 g.fillRect(x+20, y+108,5,8 );
-                 g.fillRect(x+30, y+108,5,11 );
-                 g.fillRect(x+30, y+118,20,2 );
-                 g.fillRect(x+50, y+112,4,8 );
-                 //shell
-                 g.fillRect(x+30, y+40,10,5 );
-                  g.fillRect(x+40, y+45,5,5 );
-                   g.fillRect(x+25, y+45,5,5 );
-                   g.fillRect(x+45, y+50,5,10 );
-                   g.fillRect(x+50, y+60,5,20 );
-                   g.fillRect(x+55, y+80,5,18 );
-               
-                   g.fillRect(x+45, y+65,5,5 );
-                   g.fillRect(x+25, y+85,5,5 );
-                   g.fillRect(x+30, y+80,5,5 );
-                   g.fillRect(x+35, y+75,5,5 );
-                   g.fillRect(x+40, y+70,5,5 );
-                   g.fillRect(x+20, y+90,5,5 );
-                   g.fillRect(x+15, y+95,5,5 );
-                    g.fillRect(x+45, y+93,5,5 );
-                      g.fillRect(x+40, y+88,5,5 );
-                        g.fillRect(x+35, y+83,5,5 );
-                        g.fillRect(x+30, y+78,5,5 );
-                        g.fillRect(x+25, y+73,5,5 );
-                        g.fillRect(x+20, y+68,5,5 );
-                        g.fillRect(x+15, y+63,5,5 );
-                         g.fillRect(x+10, y+58,5,5 );
-                        g.fillRect(x+15, y+43,5,5 );
-                        //continue foot
-    Graphics2D g3 = (Graphics2D) g;
-    g3.setStroke(new BasicStroke(3));
-    g3.drawLine(x-8,y+118,x+20,y+113);   //thick
-                   
-                  
-                     
-                    
-                 
+        g.fillRect(x+10, y+5,5,20 );
+        //head
+        g.fillRect(x+15, y+15, 5, 5);
+        g.fillRect(x+20, y+20,5,5 );
+        g.fillRect(x+25, y+25,5,15 );
+        g.fillRect(x+20, y+40,5,10 );
+        g.fillRect(x+15, y+50,5,8 );
+        g.fillRect(x+10, y+58,5,15 );
+        g.fillRect(x+5, y+73,5,30 );
+        g.fillRect(x+10, y+98,5,5 );
+        g.fillRect(x+10, y+103,10,5 );
+        g.fillRect(x+20, y+108,30,5 );
+        g.fillRect(x+50, y+98,5,10 );
+        g.fillRect(x-15, y+35,5,25 );
+        g.fillRect(x-10, y+60,5,5 );
+        g.fillRect(x-5, y+65,4,10 );
+        g.fillRect(x-1, y+55,4,10 );
+        g.fillRect(x+3, y+50,4,7 );
+        g.fillRect(x-5, y+39,5,5 );
+        g.fillRect(x-1, y+75,4,10 );
+        g.fillRect(x+2, y+85,5,5 );
+
+        //legs
+
+        g.fillRect(x, y+103,5,5 );
+        g.fillRect(x-5, y+108,5,5 );
+        g.fillRect(x-10, y+113,5,5 );
+        g.fillRect(x+15, y+98,5,5 );
+
+
+        g.fillRect(x+20, y+108,5,8 );
+        g.fillRect(x+30, y+108,5,11 );
+        g.fillRect(x+30, y+118,20,2 );
+        g.fillRect(x+50, y+112,4,8 );
+        //shell
+        g.fillRect(x+30, y+40,10,5 );
+        g.fillRect(x+40, y+45,5,5 );
+        g.fillRect(x+25, y+45,5,5 );
+        g.fillRect(x+45, y+50,5,10 );
+        g.fillRect(x+50, y+60,5,20 );
+        g.fillRect(x+55, y+80,5,18 );
+
+        g.fillRect(x+45, y+65,5,5 );
+        g.fillRect(x+25, y+85,5,5 );
+        g.fillRect(x+30, y+80,5,5 );
+        g.fillRect(x+35, y+75,5,5 );
+        g.fillRect(x+40, y+70,5,5 );
+        g.fillRect(x+20, y+90,5,5 );
+        g.fillRect(x+15, y+95,5,5 );
+        g.fillRect(x+45, y+93,5,5 );
+        g.fillRect(x+40, y+88,5,5 );
+        g.fillRect(x+35, y+83,5,5 );
+        g.fillRect(x+30, y+78,5,5 );
+        g.fillRect(x+25, y+73,5,5 );
+        g.fillRect(x+20, y+68,5,5 );
+        g.fillRect(x+15, y+63,5,5 );
+        g.fillRect(x+10, y+58,5,5 );
+        g.fillRect(x+15, y+43,5,5 );
+        //continue foot
+        Graphics2D g3 = (Graphics2D) g;
+        g3.setStroke(new BasicStroke(3));
+        g3.drawLine(x-8,y+118,x+20,y+113);   //thick
     }
+
     @Override
     public void actionPerformed(ActionEvent e) {
         
