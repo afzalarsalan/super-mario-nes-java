8afb8f66ff68b68727e445d3c2508d9b7bac19cf
afzal.arsalan@gmail.com <afzal.arsalan@gmail.com@03dedc1a-bcf9-4cce-036f-12a59c4ec702>
2014-05-19 05:11:49 +0000

    Removed main method from Game.class as it is called from Main Menu

--- SuperMarioNES/trunk/src/com/mario/Game.java
+++ SuperMarioNES/trunk/src/com/mario/Game.java
@@ -93,8 +93,8 @@ public class Game extends Canvas implements Runnable{
         bufferStrategy.show();
     }
 
-    public static void main(String[] args){
+    /*public static void main(String[] args){
         Game game = new Game();
         game.start();
-    }
+    }*/
 }
